b1897f62e013d7bf849a18cae4606adb4b45cd5d
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-05 20:55:24 -0600

    optimized file

--- components/Signup.js
+++ components/Signup.js
@@ -5,8 +5,8 @@ import { Link, useHistory } from "react-router-dom"
 
 export default function Signup() {
   const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
+  const passRef = useRef()
+  const passConfirmRef = useRef()
   const { signup } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
@@ -15,14 +15,12 @@ export default function Signup() {
   async function handleSubmit(e) {
     e.preventDefault()
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
-    }
+    if (passRef.current.value !== passConfirmRef.current.value) return setError("Passwords do not match")
 
     try {
       setError("")
       setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
+      await signup(emailRef.current.value, passRef.current.value)
       history.push("/")
     } catch {
       setError("Failed to create an account")
@@ -44,11 +42,11 @@ export default function Signup() {
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+              <Form.Control type="password" ref={passRef} required />
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
+              <Form.Control type="password" ref={passConfirmRef} required />
             </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
               Sign Up
