61dd152fb33ae17a66ff236285f390039acd943d
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-05 21:47:27 -0600

    some improvements

--- components/Firstpage.js
+++ components/Firstpage.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
 function Firstpage() {
-    return (
-        <div>
-            <h1>This is my first page!!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is my first page!!</h1>
+    </div>
+  )
 }
 
 export default Firstpage
