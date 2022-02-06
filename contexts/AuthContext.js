a9f7b988b0a601ff4d5fdf2c162a5734432ddf38
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-02-05 21:37:37 -0600

    context changed

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,42 +1,25 @@
-import React, { useContext, useState, useEffect } from "react"
-import { auth } from "../firebase"
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
 
 const AuthContext = React.createContext()
 
-export function useAuth() {
-  return useContext(AuthContext)
-}
+export const useAuth = () => useContext(AuthContext)
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState()
   const [loading, setLoading] = useState(true)
 
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
-  }
-
-  function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password)
-  }
-
-  function logout() {
-    return auth.signOut()
-  }
-
-  function resetPassword(email) {
-    return auth.sendPasswordResetEmail(email)
-  }
-
-  function updateEmail(email) {
-    return currentUser.updateEmail(email)
-  }
-
-  function updatePassword(password) {
-    return currentUser.updatePassword(password)
-  }
+  // * Cambié las funciones normales por funciones flecha
+  // * Esto hace que el código tenga menos líneas y sea más mantenible
+  const signup = (email, password) => auth.createUserWithEmailAndPassword(email, password)
+  const login = (email, password) => auth.signInWithEmailAndPassword(email, password)
+  const logout = () => auth.signOut()
+  const resetPassword = (email) => auth.sendPasswordResetEmail(email)
+  const updateEmail = (email) => currentUser.updateEmail(email)
+  const updatePassword = (password) => currentUser.updatePassword(password)
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user)
       setLoading(false)
     })
@@ -51,12 +34,8 @@ export function AuthProvider({ children }) {
     logout,
     resetPassword,
     updateEmail,
-    updatePassword
+    updatePassword,
   }
 
-  return (
-    <AuthContext.Provider value={value}>
-      {!loading && children}
-    </AuthContext.Provider>
-  )
+  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>
 }
